a20b1e21a488a494a329dd90c331e74c3218f832
jrasmussenjr <rasmussenjoejr@gmail.com>
2020-01-10 16:55:24 -0800

    Beginning Structure containing base dependencies

---
+++ build/precache-manifest.6807851c482bb777e440a6d26f6c02e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a398846fb28976072be98506f26d65dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b6105c4d4eae154824c",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "808a5d5c64320b21c39d",
+    "url": "/static/js/2.35e891fa.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.35e891fa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5b6105c4d4eae154824c",
+    "url": "/static/js/main.db12142c.chunk.js"
+  },
+  {
+    "revision": "647a3d0ef40eabd3afed",
+    "url": "/static/js/runtime-main.ea6aac96.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
