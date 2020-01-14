949ac4edcab5720cd0f20f10a9c906946cd570a2
jrasmussenjr <rasmussenjoejr@gmail.com>
2020-01-14 00:14:28 -0800

    Changed default view

--- src/App.js
+++ src/App.js
@@ -5,17 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <body>
+            Fill in files using the established React Components.
+        </body>
       </header>
     </div>
   );
