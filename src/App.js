e057b0c57c4ce4aabf6f24b8324fb3a7e0673c4e
jrasmussenjr <rasmussenjoejr@gmail.com>
2020-01-14 00:29:21 -0800

    Added React Router Dom and created components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,59 @@
 import React from 'react';
 import './App.css';
+import Amplify, { Auth, Hub } from 'aws-amplify';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
+  return (
+    <Router>
+      <div id="routeDiv">
+        <Switch>
+          <Route path="/test">
+            <Test />
+          </Route>
+          <Route path="/test2">
+            <Test2 />
+          </Route>
+          <Route path="/">
+            <Home />
+          </ Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}  
+   
+function Home() {
   return (
     <div className="App">
       <header className="App-header">
         <body>
-            Fill in files using the established React Components.
+          Fill in files using the established React Components.
         </body>
       </header>
     </div>
   );
+}
+
+function Test1() {
+   return (
+     <div>
+       This is Test1.
+     </div>
+   );
+}
+
+function Test2() {
+   return (
+     <div>
+       This is Test2.
+     </div>
+   );
 }
 
 export default App;
