738926d00f53e832ba3ca51ca53c0004708c4833
Anthony Nguyen <anthonydtnguyen@gmail.com>
2020-02-12 18:11:44 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -83,8 +83,8 @@ class App extends React.Component {
       const res = await checkUser();
       console.log("User is " + JSON.stringify(res));
       Auth.currentSession().then(data => console.log(data)).catch(err => console.log(err));
-      const result = await Auth.currentSession();
       this.setState({user: res});
+      const result = await Auth.currentSession();
       AWS.config.region ='us-east=1';
       AWS.config.credentials = new AWS.CognitoIdentityCredentials({
         IdentityPoolId: 'us-east-1:452e5811-58e7-4cce-8b39-90db30a8eba3',
