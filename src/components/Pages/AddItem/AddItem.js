89f00fa32d222596e32379f7d50b625554b17a13
Anthony Nguyen <anthonydtnguyen@gmail.com>
2020-02-12 19:55:06 -0800

    Update AddItem.js

--- src/components/Pages/AddItem/AddItem.js
+++ src/components/Pages/AddItem/AddItem.js
@@ -28,7 +28,7 @@ export default class AddItem extends React.Component {
     handleSubmit(event) {
         alert('A name was submitted: ' + this.state.value);
         const file = this.state.file;
-        Storage.put(this.state.value, file, {
+        Storage.put('example1.png', file, {
             level: 'private',
             contentType: 'image/png'
         })
