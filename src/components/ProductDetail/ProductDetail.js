922a48beb238d354ad868a2cf1e9b1f8e90afa11
jrasmussenjr <rasmussenjoejr@gmail.com>
2020-02-15 23:52:07 -0800

    reset test issues

--- src/components/ProductDetail/ProductDetail.js
+++ src/components/ProductDetail/ProductDetail.js
@@ -51,7 +51,7 @@ const ProductDetail = (props) => {
         const fetchData = async () => {
             try {
                 const result = await API.graphql(graphqlOperation(getItemTable, {itemID: 0})).then(e =>
-                    console.log(e)
+                    console.log(e.category)
                 );
             } catch(e) {
                 console.log(e);
