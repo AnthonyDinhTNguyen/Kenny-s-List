05f6d462f17fd40109f535cb344176da1b0cf036
jyeontaek <yeontaekj@gmail.com>
2020-02-15 22:41:35 -0800

    Clean up schema to resolve on ItemTable

--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -1,42 +1,39 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
-export const createMyType = `mutation CreateMyType(
-  $input: CreateMyTypeInput!
-  $condition: ModelMyTypeConditionInput
-) {
-  createMyType(input: $input, condition: $condition) {
-    id
-    title
-    content
-    price
-    rating
+export const createItemTable = /* GraphQL */ `
+  mutation CreateItemTable($input: CreateItemTableInput!) {
+    createItemTable(input: $input) {
+      itemID
+      category
+      description
+      images
+      name
+      postTime
+    }
   }
-}
 `;
-export const updateMyType = `mutation UpdateMyType(
-  $input: UpdateMyTypeInput!
-  $condition: ModelMyTypeConditionInput
-) {
-  updateMyType(input: $input, condition: $condition) {
-    id
-    title
-    content
-    price
-    rating
+export const updateItemTable = /* GraphQL */ `
+  mutation UpdateItemTable($input: UpdateItemTableInput!) {
+    updateItemTable(input: $input) {
+      itemID
+      category
+      description
+      images
+      name
+      postTime
+    }
   }
-}
 `;
-export const deleteMyType = `mutation DeleteMyType(
-  $input: DeleteMyTypeInput!
-  $condition: ModelMyTypeConditionInput
-) {
-  deleteMyType(input: $input, condition: $condition) {
-    id
-    title
-    content
-    price
-    rating
+export const deleteItemTable = /* GraphQL */ `
+  mutation DeleteItemTable($input: DeleteItemTableInput!) {
+    deleteItemTable(input: $input) {
+      itemID
+      category
+      description
+      images
+      name
+      postTime
+    }
   }
-}
 `;
--- src/graphql/subscriptions.js
+++ src/graphql/subscriptions.js
@@ -1,33 +1,75 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
-export const onCreateMyType = `subscription OnCreateMyType {
-  onCreateMyType {
-    id
-    title
-    content
-    price
-    rating
+export const onCreateItemTable = /* GraphQL */ `
+  subscription OnCreateItemTable(
+    $itemID: Int
+    $category: String
+    $description: String
+    $images: [String]
+    $name: String
+  ) {
+    onCreateItemTable(
+      itemID: $itemID
+      category: $category
+      description: $description
+      images: $images
+      name: $name
+    ) {
+      itemID
+      category
+      description
+      images
+      name
+      postTime
+    }
   }
-}
 `;
-export const onUpdateMyType = `subscription OnUpdateMyType {
-  onUpdateMyType {
-    id
-    title
-    content
-    price
-    rating
+export const onUpdateItemTable = /* GraphQL */ `
+  subscription OnUpdateItemTable(
+    $itemID: Int
+    $category: String
+    $description: String
+    $images: [String]
+    $name: String
+  ) {
+    onUpdateItemTable(
+      itemID: $itemID
+      category: $category
+      description: $description
+      images: $images
+      name: $name
+    ) {
+      itemID
+      category
+      description
+      images
+      name
+      postTime
+    }
   }
-}
 `;
-export const onDeleteMyType = `subscription OnDeleteMyType {
-  onDeleteMyType {
-    id
-    title
-    content
-    price
-    rating
+export const onDeleteItemTable = /* GraphQL */ `
+  subscription OnDeleteItemTable(
+    $itemID: Int
+    $category: String
+    $description: String
+    $images: [String]
+    $name: String
+  ) {
+    onDeleteItemTable(
+      itemID: $itemID
+      category: $category
+      description: $description
+      images: $images
+      name: $name
+    ) {
+      itemID
+      category
+      description
+      images
+      name
+      postTime
+    }
   }
-}
 `;
