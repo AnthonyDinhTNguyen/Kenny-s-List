0148b392a7f0e6d4bb7b263f9d941eafab5b6558
khzheng24 <kennyzheng24998@gmail.com>
2020-02-21 11:30:28 -0800

    Reduce image warping

--- src/components/Product/StaticProduct.js
+++ src/components/Product/StaticProduct.js
@@ -5,8 +5,8 @@ const Product = (props) => {
     return (
         <div className="col-lg-4 col-md-6 mb-4">
             <div className="card h-100 product">
-                <a className="product__link">
-                    <img className="card-img-top product__img" src={props.img}/>
+                <a className="product__link" style={{textAlign: "center"}}>
+                    <img className="card-img-top product__img" src={props.img} style={{width: "100%", height: "50%"}}/>
                 </a>
                 <div className="card-body product__text">
                     <h5 className="product__price">
