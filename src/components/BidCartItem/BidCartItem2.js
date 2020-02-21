b37c727657b12da6be293da22b1f1ff443279177
anthonydtnguyen <37089424+anthonydtnguyen@users.noreply.github.com>
2020-02-21 07:15:39 -0800

    Adding Bidcart

--- src/components/BidCartItem/BidCartItem2.js
+++ src/components/BidCartItem/BidCartItem2.js
@@ -11,7 +11,7 @@ import {Auth} from 'aws-amplify';
 export default class BidCartItem2 extends React.Component {
     constructor(props){
         super(props);
-        this.state ={stripe:false,stripeP:''};
+        this.state ={stripe:false,stripeP:'',clientID=''};
         this.startStripe = this.startStripe.bind(this);
     }
 
@@ -19,31 +19,16 @@ export default class BidCartItem2 extends React.Component {
         
     }
     async startStripe(event){
-/*       const url = `https://api.stripe.com/v1/payment_intents`
-      const auth = {
-        username:'sk_test_Eylua5wcXgHoTlHYk0Og1upe00ENr8WpAi',
-        password:''
-      };
-      let data = {};
-      data['payment_method_types[]']= 'card';
-      data['amount']=1000;
-      data['currency']='usd';
-      data['transfer_data[destination]']='acct_1GEXPGKzFt6viajs'
-      let test = await axios.post(url,data,{auth:{
-        username:'sk_test_Eylua5wcXgHoTlHYk0Og1upe00ENr8WpAi',
-        password:''
-      }});
-      console.log(test); */
       let url = "https://in8hc6wee5.execute-api.us-east-1.amazonaws.com/stripe/stripe-payment";
       let amount = "500";
-      let test = this.props.yourBid.toString();
+      let test = (parseFloat(this.props.yourBid)*100).toString();
       console.log(test);
       let accountID = "acct_1GEXPGKzFt6viajs";
       let postThis = url+"?amount="+amount+"&accountID="+accountID;
-      console.log(postThis);
       const stripePromise = loadStripe("pk_test_NedNuvs9YOl1WOhanD0xfJtX00q2eAowF8");
-      let logThis = await axios.get(postThis);
-      console.log(logThis);
+      let dataJSON = await axios.get(postThis);
+      console.log(dataJSON.data.body.clientSecret+"bidcart");
+      this.setState({clientID:dataJSON.data.body.clientSecret});
       this.setState({stripeP:stripePromise})
       this.setState({stripe:true});
     }
@@ -68,7 +53,7 @@ export default class BidCartItem2 extends React.Component {
                 {(this.state.stripe) ? (<div style={{position: "fixed", top: 0, left: 0, width: "100%", height: "100%", zIndex: 1, backgroundColor: "rgba(0,0,0,0.25)"}}>
                   <div style={{backgroundColor: "white", border: "2px solid black", height: "50%", width: "50%", margin: "0 auto", marginTop: 150, padding: "10px 20px 10px 20px"}}>
                     <Elements stripe={this.state.stripeP}>
-                      <CheckoutForm />
+                      <CheckoutForm clientInfo={this.state.clientID}/>
                     </Elements>
                   </div>
                 </div>) : <br />}
