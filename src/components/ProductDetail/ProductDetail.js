e13e2ebd33076641e1a70db8ef0970aed8a08700
Leroy-S1 <leroyle.ll@gmail.com>
2020-02-21 02:22:43 -0800

    testing

--- src/components/ProductDetail/ProductDetail.js
+++ src/components/ProductDetail/ProductDetail.js
@@ -47,10 +47,10 @@ const ProductDetail = (props) => {
         return formattedTime;
     };
 
-    useEffect(() => {
+    useEffect(async() => {
         try {
             setError(null);
-            Auth.currentAuthenticatedUser({
+            await Auth.currentAuthenticatedUser({
                 bypassCache: false  // Optional, By default is false. If set to true, this call will send a request to Cognito to get the latest user data
             }).then(user => {
                 setUsername(user.username);
