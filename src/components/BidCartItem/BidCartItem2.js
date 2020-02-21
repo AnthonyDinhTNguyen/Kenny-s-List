c8eece9dd7c867b587172c82537774f9cbcdb96c
anthonydtnguyen <37089424+anthonydtnguyen@users.noreply.github.com>
2020-02-21 11:26:42 -0800

    Update BidCartItem2.js

--- src/components/BidCartItem/BidCartItem2.js
+++ src/components/BidCartItem/BidCartItem2.js
@@ -27,7 +27,9 @@ export default class BidCartItem2 extends React.Component {
       console.log(amount);
       let user = (await Auth.currentAuthenticatedUser()).username;
       let response = await API.graphql(graphqlOperation(getKennysListUserTable, {username: user}));
+      console.log(response);
       let accountID = response.getKennysListUserTable.accountID;
+      console.log(accountID);
       let postThis = url+"?amount="+amount+"&accountID="+accountID;
       const stripePromise = loadStripe("pk_test_NedNuvs9YOl1WOhanD0xfJtX00q2eAowF8");
       let dataJSON = await axios.get(postThis);
