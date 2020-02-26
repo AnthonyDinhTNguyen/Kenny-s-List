7ccffc56124dc5bf98e3f1caea4cb493e3265629
jrasmussenjr <rasmussenjoejr@gmail.com>
2020-02-25 18:37:17 -0800

    Update aws-exports.js

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -3,8 +3,8 @@
 const awsmobile = {
     "aws_appsync_graphqlEndpoint": "https://j6ngyh5f45albosbh3sdjj4szm.appsync-api.us-east-1.amazonaws.com/graphql",
     "aws_appsync_region": "us-east-1",
-   	"aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-5ze6sharjrbklo5xrf4r5r5cnq"
+   	"aws_appsync_authenticationType": "AMAZON_COGNITO_USER_POOLS",
+    "aws_appsync_apiKey": "null"
 };
 
 
