b2a66ac5725facd86b5b32e2a4fe5e89483b041e
Leroy-S1 <leroyle.ll@gmail.com>
2020-03-05 22:40:49 -0800

    fix refresh issue

--- src/components/Pages/ProductDetailPage/ProductDetailPage.js
+++ src/components/Pages/ProductDetailPage/ProductDetailPage.js
@@ -4,39 +4,43 @@ import ProductDetailComponent from '../../ProductDetail/ProductDetail';
 import ProductSlider from "../../ProductSlider/ProductSlider";
 import API, {graphqlOperation} from "@aws-amplify/api";
 import {listItemTables} from "../../../graphql/queries";
-const ProductDetail = (props) => {
+// const ProductDetail = (props) => {
 
-    const [itemImg, setItemImg] = useState({});
-    const [products, setProducts] = useState({});
-    useEffect(() => {
-        const fetchData = async () => {
-            let itemsTuple = [];
-            await (API.graphql(graphqlOperation(listItemTables, {limit: 100})).then(e => {
-                itemsTuple = e.data.listItemTables.items;
-                console.log("1", itemsTuple);
-                console.log("2", props.match.params.id);
-                const product1 = itemsTuple.find(prod => prod.itemID === props.match.params.id);
-                setProducts(itemsTuple);
-                setItemImg(product1);
-            }));
+//     const [itemImg, setItemImg] = useState({});
+//     const [products, setProducts] = useState({});
+//     useEffect(() => {
+//         const fetchData = async () => {
+//             let itemsTuple = [];
+//             await (API.graphql(graphqlOperation(listItemTables, {limit: 100})).then(e => {
+//                 itemsTuple = e.data.listItemTables.items;
+//                 const product1 = itemsTuple.find(prod => prod.itemID === props.match.params.id);
+//                 setProducts(itemsTuple);
+//                 setItemImg(product1);
+//             }));
             
-        };
-        fetchData();
-    }, []);
+//         };
+//         fetchData();
+//     }, []);
 
+class ProductDetail extends Component {
+    
+    
+render(){
+    let item = props.product;
     return (
         <div className="container" style={{padding: '6rem 0'}}>
            
             <div className="card">
                 <div className="row">
                     {console.log("Adsf",itemImg.images)}
-                    {console.log("daf",props.product.images)}
-                    <ProductSlider images={props.product.images}/>
+                    {console.log("daf",item.images)}
+                    <ProductSlider images={item.images}/>
                     <ProductDetailComponent product={props.product}/>
                 </div>
             </div>
         </div>
     );
+}
 };
 
 const mapStateToProps = (state, props) =>  {
@@ -46,8 +50,6 @@ const mapStateToProps = (state, props) =>  {
         product
     }
 };
-
-
 
 export default connect(mapStateToProps, null)(ProductDetail);
 // export default ProductDetail;
