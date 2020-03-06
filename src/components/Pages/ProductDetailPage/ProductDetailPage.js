e630b1885a7c0a713d758e97fe89e8890bea169b
Leroy-S1 <leroyle.ll@gmail.com>
2020-03-05 19:26:54 -0800

    bugs fixed

--- src/components/Pages/ProductDetailPage/ProductDetailPage.js
+++ src/components/Pages/ProductDetailPage/ProductDetailPage.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 import {connect} from 'react-redux';
 import ProductDetailComponent from '../../ProductDetail/ProductDetail';
 import ProductSlider from "../../ProductSlider/ProductSlider";
