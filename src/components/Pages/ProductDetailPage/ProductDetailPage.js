505c14e66d44fd3bea4b5f7bd11cad6f43e4620e
Leroy-S1 <leroyle.ll@gmail.com>
2020-03-05 18:59:21 -0800

    testing

--- src/components/Pages/ProductDetailPage/ProductDetailPage.js
+++ src/components/Pages/ProductDetailPage/ProductDetailPage.js
@@ -3,14 +3,16 @@ import {connect} from 'react-redux';
 import ProductDetailComponent from '../../ProductDetail/ProductDetail';
 import ProductSlider from "../../ProductSlider/ProductSlider";
 const ProductDetail = (props) => {
+    const [img, setImg] = useState('');
+    setImg(props.product.images);
 
-   console.log("product", props.product.images);
+    console.log("product", img);
     
     return (
         <div className="container" style={{padding: '6rem 0'}}>
             <div className="card">
                 <div className="row">
-                    <ProductSlider images={props.product.images}/>
+                    <ProductSlider images={img}/>
                     <ProductDetailComponent product={props.product}/>
                 </div>
             </div>
