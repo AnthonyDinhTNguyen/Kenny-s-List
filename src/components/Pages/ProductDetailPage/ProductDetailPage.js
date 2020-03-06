15f2c35ee325f7a69c065c61c2a29ff6b369d515
Leroy-S1 <leroyle.ll@gmail.com>
2020-03-05 19:23:40 -0800

    getting item from db

--- src/components/Pages/ProductDetailPage/ProductDetailPage.js
+++ src/components/Pages/ProductDetailPage/ProductDetailPage.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React,{useState} from 'react';
 import {connect} from 'react-redux';
 import ProductDetailComponent from '../../ProductDetail/ProductDetail';
 import ProductSlider from "../../ProductSlider/ProductSlider";
+import API, {graphqlOperation} from "@aws-amplify/api";
+import {listItemTables} from "../../../graphql/queries";
 const ProductDetail = (props) => {
-    const imgURL = props.product.images;
+
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        const fetchData = async () => {
+            await (API.graphql(graphqlOperation(listItemTables, {limit: 100})).then(e => {
+                setProducts(e.data.listItemTables.items);
+            }).catch(e => {console.log("Failed to retrieve data");}));
+
+        };
+        fetchData();
+    }, []);
+
+    const product1 = products.find(prod => prod.itemID === props.match.params.id);
 
     return (
         <div className="container" style={{padding: '6rem 0'}}>
             <div className="card">
                 <div className="row">
-                    <ProductSlider images={imgURL}/>
-                    <ProductDetailComponent product={props.product}/>
+                    <ProductSlider images={product1.images}/>
+                    <ProductDetailComponent product={product1}/>
                 </div>
             </div>
         </div>
