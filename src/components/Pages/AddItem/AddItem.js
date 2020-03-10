426dabeb48b18e8ab4932840afd88e94df0aafea
Anthony Nguyen <anthonydtnguyen@gmail.com>
2020-03-10 16:16:50 -0700

    Update AddItem.js

--- src/components/Pages/AddItem/AddItem.js
+++ src/components/Pages/AddItem/AddItem.js
@@ -161,7 +161,7 @@ export default class AddItem extends React.Component {
 
         let link = "https://connect.stripe.com/express/oauth/authorize?client_id=ca_Glz8Mb09LGrSthPbSj28gU0WsDX65f6g&state="+magicString;
         window.open(link);
-        await API.graphql(graphqlOperation(createKennysListUserTable, {input:{username: user,randstring:magicString}})).catch(e=>
+        API.graphql(graphqlOperation(createKennysListUserTable, {input:{username: user,randstring:magicString}})).catch(e=>
             API.graphql(graphqlOperation(updateKennysListUserTable, {input:{
             username: user,
             randstring: magicString
